c93aaaff766d0e399edf377cdb254d5c2877fa78
Naomi <33375791+Henvy-Mango@users.noreply.github.com>
2023-09-04 11:31:04 +0800

    chore: 🔨 update prettier rule

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -9,23 +9,23 @@ module.exports = {
   // 在语句末尾打印分号
   semi: false,
   // 使用单引号而不是双引号
-  singleQuote: true,
+  singleQuote: false,
   // 更改引用对象属性的时间 可选值"<as-needed|consistent|preserve>"
-  quoteProps: 'as-needed',
+  quoteProps: "as-needed",
   // 在JSX中使用单引号而不是双引号
   jsxSingleQuote: false,
   // 多行时尽可能打印尾随逗号。（例如，单行数组永远不会出现逗号结尾。） 可选值"<none|es5|all>"，默认all
-  trailingComma: 'all',
+  trailingComma: "all",
   // 在对象文字中的括号之间打印空格
   bracketSpacing: true,
   // 将 > 的多行HTML元素放在最后一行的末尾，而不是单独放在下一行(不适用于自关闭元素)。
   bracketSameLine: false,
   // 在单独的箭头函数参数周围包括括号 always：(x) => x \ avoid：x => x
-  arrowParens: 'always',
+  arrowParens: "always",
   // 使用默认的折行标准 always\never\preserve
-  proseWrap: 'preserve',
+  proseWrap: "preserve",
   // 指定HTML文件的全局空格敏感度 css\strict\ignore
-  htmlWhitespaceSensitivity: 'css',
+  htmlWhitespaceSensitivity: "css",
   // 换行符使用 lf 结尾是 可选值"<auto|lf|crlf|cr>"
-  endOfLine: 'lf',
+  endOfLine: "lf",
 }
