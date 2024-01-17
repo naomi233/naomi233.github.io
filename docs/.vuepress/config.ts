c323777cb3f0a0a4c7643d6c556b87c9cf93869b
Naomi <mangogo.naomi@qq.com>
2024-01-17 18:13:28 +0800

    fix(theme): :bug: compatible with vuepress rc.11

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -15,24 +15,5 @@ export default defineUserConfig({
       `window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', 'G-37N7CLH1B1');`,
     ],
   ],
-  plugins: [
-    searchProPlugin({
-      // 索引全部内容
-      indexContent: true,
-      // 为分类和标签添加索引
-      customFields: [
-        {
-          name: "category",
-          getter: (page) => page.frontmatter.category,
-          formatter: "分类：$content",
-        },
-        {
-          name: "tag",
-          getter: (page) => page.frontmatter.tag,
-          formatter: "标签：$content",
-        },
-      ],
-    }),
-  ],
   theme,
 })
