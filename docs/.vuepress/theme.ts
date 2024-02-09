69c771397cd3cec46dd8dcdb088f604b796020eb
Naomi <33375791+Henvy-Mango@users.noreply.github.com>
2024-02-09 09:17:19 +0800

    fix(theme): 🐛 search error

--- docs/.vuepress/theme.ts
+++ docs/.vuepress/theme.ts
@@ -68,17 +68,6 @@ export default hopeTheme({
     searchPro: {
       // 索引全部内容
       indexContent: true,
-      // 为分类和标签添加索引
-      customFields: [
-        {
-          getter: (page) => page.frontmatter.category,
-          formatter: "分类：$content",
-        },
-        {
-          getter: (page) => page.frontmatter.tag,
-          formatter: "标签：$content",
-        },
-      ],
     },
   },
 })
