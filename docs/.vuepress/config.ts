e8683a476c1f670a9da5d7a9b43c0b3450f15929
Naomi <33375791+Henvy-Mango@users.noreply.github.com>
2024-08-19 17:38:54 +0800

    feat: track by site

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -7,11 +7,19 @@ export default defineUserConfig({
   title: "Naomi樱花庄",
   description: "Naomi樱花庄",
   head: [
-    ["script", { async: true, src: "https://www.googletagmanager.com/gtag/js?id=G-37N7CLH1B1" }],
+    // ["script", { async: true, src: "https://www.googletagmanager.com/gtag/js?id=G-37N7CLH1B1" }],
+    // [
+    //   "script",
+    //   {},
+    //   `window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', 'G-37N7CLH1B1');`,
+    // ],
     [
       "script",
-      {},
-      `window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', 'G-37N7CLH1B1');`,
+      {
+        defer: true,
+        src: "https://track.naomi.plus/script.js",
+        "data-website-id": "0a51c413-8150-4511-9c99-87e2c17d935f",
+      },
     ],
   ],
   theme,
