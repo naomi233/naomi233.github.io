94573b8adf3e6a30c47a3ed33c41dc715365c936
Naomi <33375791+Henvy-Mango@users.noreply.github.com>
2025-04-25 15:47:04 +0800

    chore: 🔨 change tracker

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -7,27 +7,11 @@ export default defineUserConfig({
   title: "Naomi樱花庄",
   description: "Naomi樱花庄",
   head: [
-    // ["script", { async: true, src: "https://www.googletagmanager.com/gtag/js?id=G-37N7CLH1B1" }],
-    // [
-    //   "script",
-    //   {},
-    //   `window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', 'G-37N7CLH1B1');`,
-    // ],
+    ["script", { async: true, src: "https://www.googletagmanager.com/gtag/js?id=G-X1V1VJF5XQ" }],
     [
       "script",
-      {
-        defer: true,
-        src: "https://track.naomi.plus/script.js",
-        "data-website-id": "0a51c413-8150-4511-9c99-87e2c17d935f",
-      },
-    ],
-    [
-      "script",
-      {
-        defer: true,
-        src: "https://static.cloudflareinsights.com/beacon.min.js",
-        "data-cf-beacon": '{"token": "93df9f90aa91498cb012734da1794cd0"}',
-      },
+      {},
+      "window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', 'G-X1V1VJF5XQ');",
     ],
   ],
   theme,
