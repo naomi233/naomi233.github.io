d386d56a14875af9fb635c10e6dec5dd4f332e6b
Naomi <33375791+Henvy-Mango@users.noreply.github.com>
2025-06-13 15:21:52 +0800

    chore: 🔨 bump package version

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -3,7 +3,6 @@ import theme from "./theme"
 
 export default defineUserConfig({
   lang: "zh-CN",
-  locales: { "/": { lang: "zh-CN" } },
   dest: "./dist",
   title: "Naomi樱花庄",
   description: "Naomi樱花庄",
