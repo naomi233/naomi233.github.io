70dbbe4528262d841b363678fd42e69aba0403ef
Naomi <33375791+Henvy-Mango@users.noreply.github.com>
2025-11-20 17:22:07 +0800

    docs(exp): 📝 add mihomo config

--- docs/.vuepress/navbar.ts
+++ docs/.vuepress/navbar.ts
@@ -5,9 +5,5 @@ export default navbar([
   { text: "技术笔记", icon: "mdi:rocket-launch-outline", link: "/code/" },
   { text: "DIY 记录", icon: "mdi:creation", link: "/diy/" },
   { text: "生活随想", icon: "mdi:notebook-edit-outline", link: "/life/" },
-  {
-    text: "Gitea",
-    icon: "mdi:source-repository",
-    link: "https://git.naomi.plus",
-  },
+  { text: "Gitea", icon: "mdi:source-repository", link: "https://git.naomi.plus" },
 ])
